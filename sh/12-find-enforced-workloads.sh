de13ea54c71629785e1a65e2e6da10fd0f4170a6
Alex Goller <alex.goller@illumio.com>
2021-08-03 16:01:38 +0200

    Add a example query and formatter to output a list of workloads that are in full/selective/visibility_only

---
+++ sh/12-find-enforced-workloads.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# can be used to create services for IP protocols not in the official UI interface (e.g. SCTP, 6in4, etc)
+
+curl -X GET $ILO_SERVER/api/v2/orgs/$ILO_ORG/workloads \
+     -u $ILO_API_KEY:$ILO_API_TOKEN \
+     -H 'Content-Type: application/json' \
+     -H 'Accept: application/json' | \
+     jq '.[]| { hostname: .hostname, interfaces: .interfaces[].address, enforcement: .enforcement_mode} | select(.enforcement == "full" or .enforcement == "visibility_only" or .enforcement == "selective")| [.hostname,.interfaces,.enforcement]| @csv'
