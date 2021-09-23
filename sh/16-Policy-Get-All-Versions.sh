87f0c3f31afa67d84923da1f5d31cb4cb3465feb
Alex Goller <alex.goller@illumio.com>
2021-09-23 15:30:26 +0200

    missing quote

--- sh/16-Policy-Get-All-Versions.sh
+++ sh/16-Policy-Get-All-Versions.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -H 'Accept: application/json' -X GET -u $ILO_API_KEY:$ILO_API_TOKEN "$ILO_SERVER/api/v2/orgs/$ILO_ORG/sec_policy/
+curl -H 'Accept: application/json' -X GET -u $ILO_API_KEY:$ILO_API_TOKEN "$ILO_SERVER/api/v2/orgs/$ILO_ORG/sec_policy/"
